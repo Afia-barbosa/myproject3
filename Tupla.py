9e73835a8ab18f7aa6660d6a74d205b101489eae
Afia-barbosa <107432552+Afia-barbosa@users.noreply.github.com>
2022-07-04 16:23:20 -0300

    Add files via upload

---
+++ Tupla.py
@@ -0,0 +1,95 @@
+#tupla é uma lista que não pode alterara
+
+tupla1 = (3,5,6)
+tupla2 = (8,10,12)
+print(type(tupla1))
+
+#() quando não vai mudar, colocar entre parentese
+print(len(tupla1))
+tupla3 = tupla1+tupla2
+tupla3 = tupla1+tupla2
+print(tupla1)
+#não da pra mudar o índice do termo
+#pode colocar coisas duplicadas
+#pode colocar uma lista dentro da tupla
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
